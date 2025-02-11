d9f5152953a94862dfc25c8787d6d00fc6478be0
VladimirB312 <158708298+VladimirB312@users.noreply.github.com>
2025-02-11 13:32:18 +0300

    поправил страницу поиска команий

--- companies.php
+++ companies.php
@@ -4,38 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="styles/companies-styles.css">
+    <link rel="stylesheet" href="./styles/companies-styles.css">
     <title>Company info</title>
 </head>
 <body>
-<header>
-    <h1>Company info</h1>
-</header>
-<div class="container">
-    <div class="request-form">
-        <div class="request-form__fields-wrapper">
-            <input class="request-form__email" id="request_form_email" placeholder="Email" type="text"/>
-            <input class="request-form__search-button" id="request_form_search" type="submit" value="Search"/>
-            <span class="request-form__error" id="request_form_error"></span>
-        </div>
+<h1 class="title">Company info</h1>
+<div class="request-form">
+    <div class="request-form__fields-wrapper">
+        <input class="request-form__email" id="request_form_email" placeholder="Email" type="text"/>
+        <input class="request-form__search-button" id="request_form_search" type="submit" value="Search"/>
+        <span class="request-form__error" id="request_form_error"></span>
     </div>
-    <div class="result">
-        <div class="result__name-wrapper">
-            <div class="result__name-title">Company name</div>
-            <div class="result__name" id="result-name"></div>
-        </div>
-        <div class="result__phone-wrapper">
-            <div class="result__phone-title">Phone</div>
-            <div class="result__phone" id="result-phone"></div>
-        </div>
-        <div class="result__email-wrapper">
-            <div class="result__email-title">Email</div>
-            <div class="result__email" id="result-email"></div>
-        </div>
-        <div class="result__description-wrapper">
-            <div class="result__description-title">Description</div>
-            <div class="result__description" id="result-description"></div>
-        </div>
+</div>
+<div class="result">
+    <div class="result__name-wrapper">
+        <div class="result__title">Company name</div>
+        <div class="result__value" id="result-name"></div>
+    </div>
+    <div class="result__phone-wrapper">
+        <div class="result__title">Phone</div>
+        <div class="result__value" id="result-phone"></div>
+    </div>
+    <div class="result__email-wrapper">
+        <div class="result__title">Email</div>
+        <div class="result__value" id="result-email"></div>
+    </div>
+    <div class="result__description-wrapper">
+        <div class="result__title">Description</div>
+        <div class="result__value" id="result-description"></div>
     </div>
 </div>
 <script src="scripts/search.js"></script>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="styles/index-styles.css">
+    <link rel="stylesheet" href="./styles/index-styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
